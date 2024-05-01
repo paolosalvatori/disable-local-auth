bca1445c83ccfb87b8d04a64fbeed73b587182ee
Dolev Shor <69309933+dolevshor@users.noreply.github.com>
2024-05-01 10:35:27 +0300

    Update 01-storage-accounts.sh
    Removed "Disable or enable local authentication" - DisableLocalAuth not exist in Storage Account.

--- scripts/01-storage-accounts.sh
+++ scripts/01-storage-accounts.sh
@@ -15,33 +15,6 @@ for id in $ids; do
   # Get the name of the storage account
   name=$(echo $id | awk -F '/' '{print $9}')
 
-  # Disable or enable local authentication
-  if [ "$disableLocalAuth" = "true" ]; then
-    echo "Disabling local authentication for storage account [$name]..."
-  else
-    echo "Enabling local authentication for storage account [$name]..."
-  fi
-  
-  az rest --method patch \
-    --url "https://management.azure.com${id}?api-version=$apiVersion" \
-    --headers "Content-Type=application/json" \
-    --body "{\"properties\": {\"disableLocalAuth\": $disableLocalAuth}}" 1> /dev/null
-  
-  if [ $? -eq 0 ]; then
-    if [ "$disableLocalAuth" = "true" ]; then
-      echo "Successfully disabled local authentication for storage account [$name]"
-    else
-      echo "Successfully enabled local authentication for storage account [$name]"
-    fi
-  else
-    if [ "$disableLocalAuth" = "true" ]; then
-      echo "Failed to disable local authentication for storage account [$name]"
-    else
-      echo "Failed to enable local authentication for storage account [$name]"
-    fi
-    exit -1
-  fi
-
   # Disable or enable shared key access
   if [ "$allowSharedKeyAccess" = "false" ]; then
     echo "Disabling shared key access for storage account [$name]..."
